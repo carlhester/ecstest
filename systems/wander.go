d9d4630177fc46c264913619203f186d05272391
Carl Hester <crucialcarl@gmail.com>
2020-10-25 10:56:53 -0700

    fix wander bug

--- systems/wander.go
+++ systems/wander.go
@@ -12,7 +12,7 @@ func WanderEntities(e *entity.Entity) {
 	randGen := rand.New(seed)
 
 	dir := []string{"North", "South", "East", "West"}
-	chosen := randGen.Intn(3)
+	chosen := randGen.Intn(4)
 	if e.HasComponent("wander") {
 		fmt.Printf("WANDER: %s wanders to the %s\n", e.Name, dir[chosen])
 	}
